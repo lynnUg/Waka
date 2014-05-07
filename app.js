09445b589ee37806d52f75258f739372e39a87a4
Lynn Asiimwe <lynnasiimwe@gmail.com>
2014-05-07 23:44:00 +0300

    initial commit!

--- app.js
+++ app.js
@@ -1,44 +1,105 @@
 // dependencies
 var fs = require('fs');
-var http = require('http');
 var express = require('express');
 var routes = require('./routes');
 var path = require('path');
-var mongoose = require('mongoose');
-
-// global config
 var app = express();
-app.set('port', process.env.PORT || 1337);
+var config = require('./oauth.js')
+var User = require('./user.js')
+var mongoose = require('mongoose')
+var passport = require('passport')
+var auth = require('./authentication.js')
+
+// connect to the database
+mongoose.connect('mongodb://localhost/passport-example');
+
+var app = express();
+
+app.configure(function() {
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.logger());
+app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.session({ secret: 'my_precious' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// env config
-app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('production', function(){
-    app.use(express.errorHandler());
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+ console.log('serializeUser: ' + user._id)
+ done(null, user._id);
 });
-
-// mongo config
-var MONGOLAB_URI= "add_your_mongolab_uri_here"
-var mongo = process.env.MONGOLAB_URI || 'mongodb://localhost/node-bootstrap3-template'
-mongoose.connect(mongo);
-
-// mongo model
-// var Model_Name = require('add_your_models_here');
+passport.deserializeUser(function(id, done) {
+ User.findById(id, function(err, user){
+     console.log(user)
+     if(!err) done(null, user);
+     else done(err, null)
+ })
+});
 
 // routes
 app.get('/', routes.index);
 app.get('/ping', routes.ping);
-
-// run server
-app.listen(app.get('port'), function(){
-  console.log('\nExpress server listening on port ' + app.get('port'));
+app.get('/account', ensureAuthenticated, function(req, res){
+User.findById(req.session.passport.user, function(err, user) {
+ if(err) {
+   console.log(err);
+ } else {
+   res.render('account', { user: user});
+ }
+})
+})
+app.get('/auth/facebook',
+passport.authenticate('facebook'),
+function(req, res){
 });
+app.get('/auth/facebook/callback',
+passport.authenticate('facebook', { failureRedirect: '/' }),
+function(req, res) {
+ res.redirect('/account');
+});
+app.get('/auth/twitter',
+passport.authenticate('twitter'),
+function(req, res){
+});
+app.get('/auth/twitter/callback',
+passport.authenticate('twitter', { failureRedirect: '/' }),
+function(req, res) {
+ res.redirect('/account');
+});
+app.get('/auth/github',
+passport.authenticate('github'),
+function(req, res){
+});
+app.get('/auth/github/callback',
+passport.authenticate('github', { failureRedirect: '/' }),
+function(req, res) {
+ res.redirect('/account');
+});
+app.get('/auth/google',
+passport.authenticate('google'),
+function(req, res){
+});
+app.get('/auth/google/callback',
+passport.authenticate('google', { failureRedirect: '/' }),
+function(req, res) {
+ res.redirect('/account');
+});
+app.get('/logout', function(req, res){
+req.logout();
+res.redirect('/');
+});
+
+// port
+app.listen(1337);
+
+// test authentication
+function ensureAuthenticated(req, res, next) {
+if (req.isAuthenticated()) { return next(); }
+res.redirect('/')
+}
